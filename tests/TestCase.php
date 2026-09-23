8d0ce6ebb2567865f3004798d3797fd6979ea4cd
Dev Raj Thapa <devrajthapa1191@gmail.com>
2026-09-23 18:54:21 +0545

    chore: remove unused database directory and boilerplate stubs

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,6 @@
 namespace Dev1191\BikramSambat\Tests;
 
 use Dev1191\BikramSambat\BikramSambatServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
@@ -11,10 +10,6 @@ class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Dev1191\\BikramSambat\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
